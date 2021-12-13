ef3173ac77d38d7380b4a7be38e9663f1c6138fb
Alejo Sagues <alejosagues@gmail.com>
2021-12-13 18:33:30 -0300

    CORS arreglado, instalé bootstrap 4 y fontawesome | TODO: Comenzar con servicio Auth para login

--- Clientes.API/Startup.cs
+++ Clientes.API/Startup.cs
@@ -45,6 +45,12 @@ namespace Clientes.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            app.UseCors(builder =>
+            {
+                builder.WithOrigins("http://localhost:4200");
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
